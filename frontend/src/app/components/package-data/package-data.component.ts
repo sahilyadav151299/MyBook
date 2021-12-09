1d59f5192f200dfe8c33bf605c25f17481f634d6
Sahil Yadav <sahilyadav151299@gmail.com>
2021-12-09 16:29:25 +0530

    User-packages working

--- frontend/src/app/components/package-data/package-data.component.ts
+++ frontend/src/app/components/package-data/package-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UserPackagesService } from 'src/app/services/user-packages.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
