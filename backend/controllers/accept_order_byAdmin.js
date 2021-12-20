8e4cf38443c8c639a04b6c3b2607d285578ba61e
Sourabh rai <sourabh_rai1@persistent.com>
2021-12-20 15:49:21 +0530

    improvement in accept orders

--- backend/controllers/accept_order_byAdmin.js
+++ backend/controllers/accept_order_byAdmin.js
@@ -13,37 +13,59 @@ exports.placedorders = async(req, res) => {
         const placedorders = await orderSchema.find({ flag: "Placed" })
             // placedorders.forEach()
         var result = [];
-        for (let i = 0; i < placedorders.length; i++) {
 
+        for (let i = 0; i < placedorders.length; i++) {
+            var books = [];
+            var categories = [];
             const customer_name = await CustomerSchema.findById(placedorders[i].customerId)
+
+            const date_ob = new Date(placedorders[i].create_at)
+
+            const date = date_ob.getDate();
+            const month = date_ob.getMonth() + 1;
+            const year = date_ob.getFullYear();
+            const add = await addressSchema.find({ customerId: placedorders[i].customerId })
             for (let j = 0; j < placedorders[i].book_rented.length; j++) {
 
 
                 const rented_book = await bookSchema.findById(placedorders[i].book_rented[j].bookId)
-                const date_ob = new Date(placedorders[i].create_at)
 
-                const add = await addressSchema.find({ customerId: placedorders[i].customerId })
+                books.push(rented_book.book_name)
 
-                const date = date_ob.getDate();
-                const month = date_ob.getMonth() + 1;
-                const year = date_ob.getFullYear();
+                categories.push(rented_book.category_name)
 
-                let newdata = {
-                    customer: customer_name.name,
-                    book_rented: rented_book,
-                    address: add,
-                    create_at: date + "-" + month + "-" + year
 
-                }
-                result.push(newdata)
+
+
+
+
+
+                // result.push(newdata)
+
+
 
             }
 
+
+            let newdata = {
+                customer: customer_name.name,
+                book_rented: books,
+                category: categories,
+                address: add,
+                create_at: date + "-" + month + "-" + year
+
+            }
+
+            books = [{}];
+            books.push("#")
+            result.push(newdata)
+
         }
+
+        console.log(result)
         res.send(result)
 
-
-        console.log(placedorders.length)// for testing purpose
+        console.log(placedorders.length) // for testing purpose
 
 
     } catch (error) {
